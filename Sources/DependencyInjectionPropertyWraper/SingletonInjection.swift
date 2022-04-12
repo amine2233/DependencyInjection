d56515a4942c912f3d6b8fc8d27bc8d0b8e835e3
Amine <amine.bensalah@intech-consulting.fr>
2022-04-12 21:37:41 +0200

    fix DependencyInjectionPropertyWraper tests

--- Sources/DependencyInjectionPropertyWraper/SingletonInjection.swift
+++ Sources/DependencyInjectionPropertyWraper/SingletonInjection.swift
@@ -12,7 +12,7 @@ import DependencyInjection
 public struct SingletonInjection<Service> {
     private var service: Service
 
-    public init(dependencies: DependencySingleton) {
+    public init(dependencies: inout DependencySingleton) {
         do {
             self.service = try dependencies.singleton()
         } catch {
@@ -32,11 +32,40 @@ public struct SingletonInjection<Service> {
 }
 
 @propertyWrapper
+public struct SingletonKeyInjection<Service> {
+    private var service: Service
+    private let key: DependencyKey
+
+    public init(key: DependencyKey, dependencies: inout DependencySingleton) {
+        self.key = key
+        do {
+            self.service = try dependencies.singleton(key)
+        } catch {
+            fatalError("Can't resolve singleton \(Service.self) using key: \(key.rawValue). Error: \(error.localizedDescription)")
+        }
+    }
+
+    public var wrappedValue: Service {
+        get { service }
+        mutating set { service = newValue }
+    }
+
+    public var projectedValue: SingletonKeyInjection<Service> {
+        get { return self }
+        mutating set { self = newValue }
+    }
+}
+
+@propertyWrapper
 public struct CreateInjection<Service: DependencyServiceType> {
     private var service: Service
 
     public init(dependencies: Dependency) {
-        self.service = dependencies.create(Service.self)
+        do {
+            self.service = try dependencies.create(Service.self)
+        } catch {
+            fatalError("Can't resolve singleton \(Service.self). Error: \(error.localizedDescription)")
+        }
     }
 
     public var wrappedValue: Service {
