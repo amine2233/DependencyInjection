ceb0c7d4ae2e65fb2617458cbb9efd91a6d4d178
Amine Bensalah <amine.bensalah@intech-consulting.fr>
2024-05-19 09:37:01 +0200

    chore: update workflow and add visionOS (#22)

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "DependencyInjection",
-    platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v6), .tvOS(.v13)],
+    platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v6), .tvOS(.v13), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# DependencyInjection & DependencyInjectionPropertyWraper
+# DependencyInjection & DependencyInjectionPropertyWraper & DependencyInjectionAutoRegistration
 
 ## Tests
 
